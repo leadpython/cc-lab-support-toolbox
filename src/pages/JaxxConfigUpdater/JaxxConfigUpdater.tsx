295a1f37945fe8d9a2ad9f1fa0f6f51d0df63a86
leadpython <jeffrey.campecino@gmail.com>
2019-10-19 19:57:05 -0700

    created react-typescript project with material ui

---
+++ src/pages/JaxxConfigUpdater/JaxxConfigUpdater.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './JaxxConfigUpdater.css';
+
+const JaxxConfigUpdater: React.FC = () => {
+  return (
+    <div className="JaxxConfigUpdater">
+        JAXX CONFIG UPDATER
+    
+    </div>
+  );
+}
+
+export default JaxxConfigUpdater;
